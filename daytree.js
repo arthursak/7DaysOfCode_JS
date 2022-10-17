7c64dfb7d6081383ac9ad5556ab7b1a5d08f5de6
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-16 22:24:49 -0300

    Adicionando desafio do dia 3 finalizado

--- daytree.js
+++ daytree.js
@@ -5,43 +5,36 @@ const escolha = prompt("Você quer seguir para área de Front-End ou seguir para
 if (escolha == 1) {
     const escolhaFront = prompt("Você quer aprender React ou aprender Vue?\nDigite 1 para React ou 2 para Vue!");
     if (escolhaFront == 1) {
-        prompt("React é bem legal! Você quer seguir se especializando na área escolhida (Front-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")
-        const escolhaTec = prompt("Legal! Qual técnologia você quer aprender mais? Digite SAIR para parar de digitar")
+        prompt("React é bem legal! Você quer seguir se especializando na área escolhida (Front-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")        
         
-        while (escolhaTec !== 'SAIR') {
-            
-            alert("Irado!")
-        } 
+    } else if (escolhaFront == 2){
+        prompt("Vue é bem legal! Você quer seguir se especializando na área escolhida (Front-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")        
+        
     } else {
-        prompt("Vue é bem legal! Você quer seguir se especializando na área escolhida (Front-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")
-        const escolhaTec = prompt("Legal! Qual técnologia você quer aprender mais?\n Digite sair para parar de digitar")
-        
-        while (escolhaTec != "sair") {
-            const linguagens = prompt("Digite a linguagem:")
-            alert(linguagens);
-            const mensagem = prompt("Irado!")
-            break
-        }
-        
+        alert("Opção invalida")
     }
-    } else {
-    const escolhaBack = prompt("Você quer aprender C# ou Java? Digite 1 para C# ou 2 para Java!");
+} else {
+    const escolhaBack = prompt("Você quer aprender C# ou Java?\nDigite 1 para C# ou 2 para Java")
+
     if (escolhaBack == 1) {
-        const escolhaC = prompt("C# é bem legal! Você quer seguir se especializando na área escolhida (Back-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")
-        const escolhaTec = prompt("Legal! Qual técnologia você quer aprender mais? Digite SAIR para parar de digitar")
-        
-        while (escolhaTec != "SAIR") {
-            const linguagensFront = prompt("Digite a linguagem:")
-            const mensagem = prompt("Irado!")
-        } 
+        prompt("C# é bem legal! Você quer seguir se especializando na área escolhida (Back-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")        
+
+    } else if (escolhaBack == 2) {
+        prompt("Java é bem legal! Você quer seguir se especializando na área escolhida (Back-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")        
+
     } else {
-        const escolhaVue = prompt("Vue é bem legal! Você quer seguir se especializando na área escolhida (Front-End) ou vai seguir se desenvolvendo para se tornar Fullstack?")
-        const escolhaTec = prompt("Legal! Qual técnologia você quer aprender mais? Digite SAIR para parar de digitar")
-        
-        while (escolhaTec != "SAIR") {
-            const linguagensFront = prompt("Digite a linguagem:")
-            const mensagem = prompt("Irado!")
-        }
+        alert("Opção invalida")
     }
+}
+
+alert("Tem alguma outra linguagem que você quer aprender?\nDigite SAIR para sair!")
+
+let msg
+while (msg !== "SAIR") {
+    prompt("Digite uma linguagem:")
+    alert("Irado!")
+    msg = prompt("Alguma outra linguagem?")
 
 }
+
+alert("Saindo!\nTenho certeza que você se dará muito bem com essas técnologias")
