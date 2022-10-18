e4a715ae20e7bd2b30d2cc7e8537bf9fee080629
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-18 20:37:16 -0300

    Adicionando desafio 6 e alterando o nome dos arquivos JS

--- day6.js
+++ day6.js
@@ -34,7 +34,53 @@ while (lista.toUpperCase() != "NÃO") {
 
     }
 
-    lista = confirm("Gostaria de adicionar mais algum alimento a sua lista?")
+    lista = prompt("Gostaria de adicionar mais algum alimento a sua lista ou remover algum alimento da lista?")
+
+    while (lista.toUpperCase() == "REMOVER") {
+        categoria = prompt("Você quer remover um alimento de qual categoria?\nDigite 1 para Frutas, 2 para laticínios, 3 para congelados e 4 para doces!")
+
+        if (categoria == 1) {
+            msg = prompt("Qual alimento você gostaria de remover da lista?")
+            if (listaFrutas.includes(msg)) {
+                var posicao = listaFrutas.indexOf(msg)
+                listaFrutas.splice(posicao)
+                
+            } else {
+                alert("Não foi possivel encontrar o alimento dentro da lista!")
+            }
+                
+        } else if (categoria == 2) {
+            msg = prompt("Qual alimento você gostaria de remover da lista?")
+            if (listaLaticinios.includes(msg)) {
+                var posicao = listaLaticinios.indexOf(msg)
+                listaLaticinios.splice(posicao)
+                
+            } else {
+                alert("Não foi possivel encontrar o alimento dentro da lista!")
+            }
+        } else if (categoria == 3) {
+            msg = prompt("Qual alimento você gostaria de remover da lista?")
+            if (listaCongelados.includes(msg)) {
+                var posicao = listaCongelados.indexOf(msg)
+                listaCongelados.splice(posicao)
+                
+            } else {
+                alert("Não foi possivel encontrar o alimento dentro da lista!")
+            }
+        } else if (categoria == 4) {
+            msg = prompt("Qual alimento você gostaria de remover da lista?")
+            if (listaDoces.includes(msg)) {
+                var posicao = listaDoces.indexOf(msg)
+                listaDoces.splice(posicao)
+                
+            } else {
+                alert("Não foi possivel encontrar o alimento dentro da lista!")
+            }
+        }
+
+        lista = prompt("Gostaria de remover mais algum alimento a sua lista ou adicionar?")
+
+    }
 
 
 }
