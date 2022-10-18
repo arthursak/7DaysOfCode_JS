b5a43d7c1388f645d8816ddcc8f4e183e70e0df9
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-17 22:09:52 -0300

    Adicionando desafio do dia 5 completo

--- dayfive.js
+++ dayfive.js
@@ -3,20 +3,30 @@ let listaLaticinios = []
 let listaCongelados = []
 let listaDoces = []
 let msg = ""
+let categoria = ""
 
 
-alert("Monte sua lista de compras!\nCaso a lista esteja completa, digite sair!")
+var lista = confirm("Bem vindo a lista de compras do Tuts\nGostaria de montar a sua?")
 
-while (msg !== "sair") {
-    msg = prompt("Quais produtos você quer inserir na lista de compras?")
-
-    if (msg === "Laranja"){
+while (lista != false) {
+    msg = prompt("Qual comida você gostaria de adicionar?")
+    categoria = prompt("Em qual categoria esse alimento se encaixa?\nDigite 1 para Frutas, 2 para laticínios, 3 para congelados e 4 para doces!")
+    
+    if (categoria == 1) {
         listaFrutas.push(msg)
-
-    } else if (msg === "Mortadela"){
+    } else if (categoria == 2) {
+        listaLaticinios.push(msg)
+    } else if (categoria == 3) {
         listaCongelados.push(msg)
+    } else if (categoria == 4) {
+        listaDoces.push(msg)
+    } else {
+        alert(`A categoria ${categoria} não é valida, escolha novamente`)
 
     }
+
+    lista = confirm("Gostaria de adicionar mais algum alimento a sua lista?")
+
 }
 
-alert(`Sua lista de compras:\nFrutas: ${listaFrutas}\nCongelados: ${listaCongelados}`)
+alert(`Sua lista de compras:\nFrutas: ${listaFrutas}\nLaticínios: ${listaLaticinios}\nCongelados: ${listaCongelados}\nDoces: ${listaDoces}`)
