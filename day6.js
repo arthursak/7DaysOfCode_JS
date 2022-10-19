2a0257e3abfd6c7bf6d8cbc3b0ecb7787a70412c
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-19 16:30:37 -0300

    Refazendo código do dia 6

--- day6.js
+++ day6.js
@@ -11,78 +11,40 @@ let listaFrutas = []
 let listaLaticinios = []
 let listaCongelados = []
 let listaDoces = []
-let msg = ""
-let categoria = ""
+let resp = ''
+let categoria = ''
+let remover = ''
 
 
-var lista = prompt("Bem vindo a lista de compras do Tuts\nGostaria de montar a sua?")
+var lista = prompt('Bem vindo a lista de compras do Tuts\nVamos montar a sua?')
 
-while (lista.toUpperCase() != "NÃO") {
-    msg = prompt("Qual comida você gostaria de adicionar?")
-    categoria = prompt("Em qual categoria esse alimento se encaixa?\nDigite 1 para Frutas, 2 para laticínios, 3 para congelados e 4 para doces!")
-    
-    if (categoria == 1) {
-        listaFrutas.push(msg)
-    } else if (categoria == 2) {
-        listaLaticinios.push(msg)
-    } else if (categoria == 3) {
-        listaCongelados.push(msg)
-    } else if (categoria == 4) {
-        listaDoces.push(msg)
+while (lista.toUpperCase() === 'sim') {
+    if (listaFrutas.length === 0 && listaLaticinios.length === 0 && listaCongelados.length === 0 && listaDoces.length === 0) {
+        montaLista()
+        lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
+
     } else {
-        alert(`A categoria ${categoria} não é valida, escolha novamente`)
-
+        lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
     }
+        
+}
 
-    lista = prompt("Gostaria de adicionar mais algum alimento a sua lista ou remover algum alimento da lista?")
+function montaLista() {
+    resp = prompt('Qual alimento você gostaria de adiconar?')
+    categoria = prompt('---------------\nEm qual categoria o alimento se encaixa?\nDigite:\n1 - Frutas\n2 - Laticínios\n3 - Congelados\n4 - Doces\n---------------')
 
-    while (lista.toUpperCase() == "REMOVER") {
-        categoria = prompt("Você quer remover um alimento de qual categoria?\nDigite 1 para Frutas, 2 para laticínios, 3 para congelados e 4 para doces!")
-
-        if (categoria == 1) {
-            msg = prompt("Qual alimento você gostaria de remover da lista?")
-            if (listaFrutas.includes(msg)) {
-                var posicao = listaFrutas.indexOf(msg)
-                listaFrutas.splice(posicao)
-                
-            } else {
-                alert("Não foi possivel encontrar o alimento dentro da lista!")
-            }
-                
-        } else if (categoria == 2) {
-            msg = prompt("Qual alimento você gostaria de remover da lista?")
-            if (listaLaticinios.includes(msg)) {
-                var posicao = listaLaticinios.indexOf(msg)
-                listaLaticinios.splice(posicao)
-                
-            } else {
-                alert("Não foi possivel encontrar o alimento dentro da lista!")
-            }
-        } else if (categoria == 3) {
-            msg = prompt("Qual alimento você gostaria de remover da lista?")
-            if (listaCongelados.includes(msg)) {
-                var posicao = listaCongelados.indexOf(msg)
-                listaCongelados.splice(posicao)
-                
-            } else {
-                alert("Não foi possivel encontrar o alimento dentro da lista!")
-            }
-        } else if (categoria == 4) {
-            msg = prompt("Qual alimento você gostaria de remover da lista?")
-            if (listaDoces.includes(msg)) {
-                var posicao = listaDoces.indexOf(msg)
-                listaDoces.splice(posicao)
-                
-            } else {
-                alert("Não foi possivel encontrar o alimento dentro da lista!")
-            }
-        }
-
-        lista = prompt("Gostaria de remover mais algum alimento a sua lista ou adicionar?")
-
+    if (categoria == 1) {
+        listaFrutas.push(resp)
+    } else if (categoria == 2) {
+        listaLaticinios.push(resp)
+    } else if (categoria == 3) {
+        listaCongelados.push(resp)
+    } else if (categoria == 4) {
+        listaDoces.push(resp)
+    } else {
+        alert(`${categoria} não é uma categoria válida`)
     }
-
-
+    
 }
 
 alert(`Sua lista de compras:\nFrutas: ${listaFrutas}\nLaticínios: ${listaLaticinios}\nCongelados: ${listaCongelados}\nDoces: ${listaDoces}`)
