7627514ac1f572d6a835f982adacbb0c050d72b9
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-20 17:29:03 -0300

    Não funcionaaaaaaa

--- day7.js
+++ day7.js
@@ -5,52 +5,44 @@ let soma
 let sub
 let mult
 let div
-
+let menu
+let result
 
 alert('--------------CalculaTuts--------------\n              Bem Vindo!              ')
-msg = prompt('Qual operação você gostaria de realizar?\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair da calculadora')
-
+msg = prompt('O que deseja fazer?\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair')
 while (msg != 5) {
-    num1 = prompt('Digite o 1º número')
-    num2 = prompt('Digite o 2º número')
+    num1 = prompt('Digite o 1º número:')
+    num2 = prompt('Digite o 2º número:')
 
-    switch (msg) {
-        case 1:
-            somar();
-            break;
-        case 2:
-            subt();
-            break;
-        case 3:
-            multi();
-            break;
-        case 4:
-            divi();
-            break;
-        case 5:
-            alert('Até mais!')
-
+    if (msg === 1) {
+        somar()
+        alert(result)
     }
-    msg = prompt('Qual outra operação você gostaria de realizar?\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair da calculadora')
+
+    msg = prompt('O que deseja fazer?\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Sair')
+
 
 }
+    
 
-function somar() {
-    soma = num1 + num2
-    console.log(soma)
-}
+    function somar() {
+        soma = num1 + num2
+        result = (`O resultado de ${num1} + ${num2} é: `,soma)
+        return result
+    }
 
-function subt() {
-    sub = num1 - num2
-    alert(sub)
-}
+    function subt() {
+        sub = num1 - num2
+        alert(sub)
+    }
 
-function multi() {
-    mult = num1 * num2
-    alert(mult)
-}
+    function multi() {
+        mult = num1 * num2
+        alert(mult)
+    }
 
-function divi() {
-    div = num1 / num2
-    alert(div)
-}
+    function divi() {
+        div = num1 / num2
+        alert(div)
+    }
+    
