c0b5dd43ac4efa540a096b0cef9ceb22e9a44da5
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-19 21:11:35 -0300

    Programa adicionando itens a lista, falta remover itens

--- day6.js
+++ day6.js
@@ -1,13 +1,5 @@
-/*
-Usar o programa da lista de compras do dia 5
-Criar opção de remover algum item da lista 
-A opção deve aparecer junto a msg de "Deseja adicionar algum alimento a lista?"
-Caso a pessoa queira remover, exibir lista e questionar qual dos alimentos a pessoa quer remover
-Após o cliente remover, exibir a lista novamente com a confirmação do item removido
-Caso o item não exista na lista, exibir erro
-*/
 
-let listaFrutas = []
+let listaFrutas = [] 
 let listaLaticinios = []
 let listaCongelados = []
 let listaDoces = []
@@ -16,35 +8,43 @@ let categoria = ''
 let remover = ''
 
 
-var lista = prompt('Bem vindo a lista de compras do Tuts\nVamos montar a sua?')
+var lista = prompt('---------------\nBem vindo a lista de compras do Tuts\nDigite:\nAdicionar - Para adicionar um item a lista\nNão - Para finalizar a lista\n---------------')
 
-while (lista.toUpperCase() === 'sim') {
+if (lista.toUpperCase() === 'ADICIONAR') {
     if (listaFrutas.length === 0 && listaLaticinios.length === 0 && listaCongelados.length === 0 && listaDoces.length === 0) {
         montaLista()
-        lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
 
-    } else {
-        lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
+    } else if (lista.toUpperCase() === 'REMOVER'){
+        removeLista()
     }
         
 }
 
 function montaLista() {
-    resp = prompt('Qual alimento você gostaria de adiconar?')
-    categoria = prompt('---------------\nEm qual categoria o alimento se encaixa?\nDigite:\n1 - Frutas\n2 - Laticínios\n3 - Congelados\n4 - Doces\n---------------')
 
-    if (categoria == 1) {
-        listaFrutas.push(resp)
-    } else if (categoria == 2) {
-        listaLaticinios.push(resp)
-    } else if (categoria == 3) {
-        listaCongelados.push(resp)
-    } else if (categoria == 4) {
-        listaDoces.push(resp)
-    } else {
-        alert(`${categoria} não é uma categoria válida`)
+    while (lista.toUpperCase() === 'ADICIONAR') {
+        resp = prompt('Qual alimento você gostaria de adiconar?')
+        categoria = prompt('---------------\nEm qual categoria o alimento se encaixa?\nDigite:\n1 - Frutas\n2 - Laticínios\n3 - Congelados\n4 - Doces\n---------------')
+
+        
+        if (categoria == 1) {
+            listaFrutas.push(resp)
+        } else if (categoria == 2) {
+            listaLaticinios.push(resp)
+        } else if (categoria == 3) {
+            listaCongelados.push(resp)
+        } else if (categoria == 4) {
+            listaDoces.push(resp)
+        } else {
+            alert(`${categoria} não é uma categoria válida`)
+        }
+        lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
+
     }
-    
+}
+
+function removeLista() {
+    alert('Removido')
 }
 
 alert(`Sua lista de compras:\nFrutas: ${listaFrutas}\nLaticínios: ${listaLaticinios}\nCongelados: ${listaCongelados}\nDoces: ${listaDoces}`)
