90e1ffa463b2913c890899f763b35a565f35061d
Arthur Murilo da Rocha <arthurmourilo@gmail.com>
2022-10-19 22:16:04 -0300

    Desafio do dia 6 finalizado com adicionar itens e remover itens

--- day6.js
+++ day6.js
@@ -6,19 +6,14 @@ let listaDoces = []
 let resp = ''
 let categoria = ''
 let remover = ''
+let posicao = ''
 
 
 var lista = prompt('---------------\nBem vindo a lista de compras do Tuts\nDigite:\nAdicionar - Para adicionar um item a lista\nNão - Para finalizar a lista\n---------------')
 
 if (lista.toUpperCase() === 'ADICIONAR') {
-    if (listaFrutas.length === 0 && listaLaticinios.length === 0 && listaCongelados.length === 0 && listaDoces.length === 0) {
-        montaLista()
-
-    } else if (lista.toUpperCase() === 'REMOVER'){
-        removeLista()
-    }
-        
-}
+    montaLista()
+    } 
 
 function montaLista() {
 
@@ -38,13 +33,68 @@ function montaLista() {
         } else {
             alert(`${categoria} não é uma categoria válida`)
         }
+
         lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
 
+        while (lista.toUpperCase() === 'REMOVER'){
+            removeLista()
+        }
     }
+        
 }
 
+
+
 function removeLista() {
-    alert('Removido')
-}
+    categoria = prompt ('Em qual categoria está o alimento que você quer remover?\n1 - Frutas\n2 - Laticínios\n3 - Congelados\n4 - Doces')
+            
+    if (categoria == 1) {
+        remover = prompt('Qual alimento você quer remover?')
+
+        if (listaFrutas.includes(remover)){
+            posicao = listaFrutas.indexOf(remover)
+            listaFrutas.splice(posicao, 1)
+        } else {
+            alert('Não foi possivel localizar o alimento!')
+            alert('Voltando ao menu...')
+        }
+        
+
+    } else if (categoria == 2) {
+        remover = prompt('Qual alimento você quer remover?')
+
+        if (listaLaticinios.includes(remover)){
+            posicao = listaLaticinios.indexOf(remover)
+            listaLaticinios.splice(posicao, 1)
+        } else {
+            alert('Não foi possivel localizar o alimento')
+        }
+        alert('Voltando ao menu...')
+    } else if (categoria == 3) {
+        remover = prompt('Qual alimento você quer remover?')
+
+        if (listaCongelados.includes(remover)){
+            posicao = listaCongelados.indexOf(remover)
+            listaCongelados.splice(posicao, 1)
+        } else {
+            alert('Não foi possivel localizar o alimento')
+        }
+    } else if (categoria == 4) {
+        remover = prompt('Qual alimento você quer remover?')
+
+        if (listaDoces.includes(remover)){
+            posicao = listaDoces.indexOf(remover)
+            listaDoces.splice(posicao, 1)
+        } else {
+            alert('Não foi possivel localizar o alimento')
+        }
+    } else {
+        alert(`${categoria} não é uma categoria válida`)
+    }
+    lista = prompt('---------------\nDigite:\nAdicionar - Para adicionar mais um item a lista\nRemover - Para remover um item da lista\nNão - Para finalizar a lista\n---------------')
+
+
+    }    
+
 
 alert(`Sua lista de compras:\nFrutas: ${listaFrutas}\nLaticínios: ${listaLaticinios}\nCongelados: ${listaCongelados}\nDoces: ${listaDoces}`)
